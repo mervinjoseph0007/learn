f29862e09fb12586b6fba0cd3fbc46a5654bcec4
Mervin Joseph <84064619+mervinjoseph0007@users.noreply.github.com>
2023-02-28 08:28:04 +0530

    Add files via upload

---
+++ SComputers/admin.php
@@ -0,0 +1,20 @@
+<?php 
+    // include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+</head>
+<body>
+    <h3>Upload Popup Image</h3>
+    <div>
+        <!-- <input type="file"/> -->
+    </div>
+</body>
+</html>
